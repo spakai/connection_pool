b34085c70583a12f966be79133a37ad528314fdc
Prakash <spakai@gmail.com>
2015-11-28 01:53:54 +0800

    Refactor test case

--- src/test/java/com/spakai/PooledConnectionTest.java
+++ src/test/java/com/spakai/PooledConnectionTest.java
@@ -2,16 +2,24 @@ package com.spakai;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class PooledConnectionTest {
+	@Mock JDBConnection mockConnection;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void LeaseTimeExpired() {
-		JDBConnection mockConnection = mock(JDBConnection.class);
 		PooledConnection pooledConn = new PooledConnection(mockConnection);
 
 		assertThat(pooledConn.isExpired(), is(false));
